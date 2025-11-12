733d93ebaa84a05b2e904ea90b4649ea60cba7d8
fatimaBenazzou <b.fati2102@gmail.com>
2025-11-12 10:02:48 +0100

    js basics

---
+++ 5.js/basics.js
@@ -0,0 +1,47 @@
+/* Exo 1 */
+
+// var input1, input2
+
+// input1 = Number(prompt("Enter a number"))
+// input2 = Number(prompt("Enter a number"))
+
+// var sum = input1 + input2
+
+// console.log("The sum of " + input1 + " and " + input2 + " is " + sum)
+
+/* Exo 2 */
+// const length = prompt("Enter the length: ")
+// const width = prompt("Enter the width: ")
+
+// const area = length * width
+
+// console.log("The area = ", area)
+
+// const gameMenu = Number(prompt(
+//   "Choose an option: 1.Start 2.Settings 3.Credits 4.Exit"
+// ));
+
+// switch (gameMenu) {
+//   case 1:
+//     console.log("Start...");
+//     break;
+//   case 2:
+//     console.log("Settings...");
+//     break;
+//   case 3:
+//     console.log("Credits...");
+//     break;
+//   case 4:
+//     console.log("Exit...");
+//     break;
+
+//   default:
+//     console.log("Error...");
+//     break;
+// }
+
+	
+// Exemple : 
+for (var i = 0; i < 10; i++) { 
+    console.log(i)
+};
