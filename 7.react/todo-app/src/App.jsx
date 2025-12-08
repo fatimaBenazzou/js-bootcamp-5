307a77ddf852c9b40293d60b68691ac66b686ca9
fatimaBenazzou <b.fati2102@gmail.com>
2025-12-08 16:21:23 +0100

    .

--- 7.react/todo-app/src/App.jsx
+++ 7.react/todo-app/src/App.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import Header from "./components/Header";
 
 export default function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    const newTheme = theme === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+  };
+
   return (
-    <div data-theme={"light"} className="bg-base-100">
-      <Header />
+    <div
+      data-theme={theme}
+      className="min-h-screen bg-base-100 bg-no-repeat"
+      style={{
+        backgroundImage: `url(/images/bg-desktop-${theme}.jpg)`,
+        backgroundSize: "100vw 100vh",
+        backgroundPosition: "top center",
+      }}
+    >
+      <Header theme={theme} toggleTheme={toggleTheme} />
     </div>
   );
 }
