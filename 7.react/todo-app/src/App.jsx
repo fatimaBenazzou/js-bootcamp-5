3838146dbb6b09bc4a7531acd2b2dedc5a94429e
fatimaBenazzou <b.fati2102@gmail.com>
2025-12-08 15:39:28 +0100

    init

---
+++ 7.react/todo-app/src/App.jsx
@@ -0,0 +1,9 @@
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <div data-theme={"light"} className="bg-base-100">
+      <Header />
+    </div>
+  );
+}
---
+++ 7.react/todo-app/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center mb-8">
+      <h1 className="text-4xl font-bold text-white tracking-wider drop-shadow-lg uppercase">
+        Todo
+      </h1>
+      <button>
+        <img src="" alt="" />
+      </button>
+    </header>
+  );
+}
