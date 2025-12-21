27f4b282822798de90d9369f4fe19087beba5e3c
fatimaBenazzou <b.fati2102@gmail.com>
2025-12-21 15:01:16 +0100

    redux toolkit init

---
+++ 7.react/todo-app-rtk/src/components/AddTodo.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function AddTodo({ addTodo }) {
+  const [newTodoText, setNewTodoText] = useState("");
+
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+
+    if (newTodoText.trim()) {
+      addTodo(newTodoText);
+      setNewTodoText("");
+    }
+  };
+  return (
+    <div className="card bg-base-100 shadow-lg p-4 mb-4 rounded-lg">
+      <form onSubmit={handleAddTodo} className="flex items-center gap-2">
+        <input
+          value={newTodoText}
+          type="text"
+          className="input input-ghost w-full flex-1"
+          placeholder="Create a new todo..."
+          onChange={(e) => setNewTodoText(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && handleAddTodo}
+        />
+        <button
+          type="submit"
+          className="btn btn-circle btn-ghost btn-sm border border-zinc-400"
+        >
+          +
+        </button>
+      </form>
+    </div>
+  );
+}
---
+++ 7.react/todo-app-rtk/src/components/TodoList.jsx
@@ -0,0 +1,76 @@
+import { useCallback, useRef, useState } from "react";
+import AddTodo from "./AddTodo";
+import EditModal from "./EditModal";
+import TodoItem from "./TodoItem";
+import SortingDropdown from "./SortingDropdown";
+import SearchBar from "./SearchBar";
+
+export default function TodoList({
+  todos,
+  addTodo,
+  toggleTodoCompletion,
+  deleteTodo,
+  editTodo,
+  setSortBy,
+  searchQuery,
+  setSearchQuery,
+}) {
+  const dialogRef = useRef(null);
+  const [dialog, setDialog] = useState({ id: null, text: "" });
+
+  const handleEdit = useCallback((id, currentValue) => {
+    setDialog({ id, text: currentValue });
+    dialogRef.current?.showModal();
+  }, []);
+
+  const saveTodo = () => {
+    if (dialog.text?.trim()) {
+      editTodo(dialog.id, dialog.text);
+      dialog.current?.close();
+    }
+  };
+
+  return (
+    <section>
+      {/* input */}
+      <AddTodo addTodo={addTodo} />
+      {/* list */}
+      <div className="card bg-base-100 shadow-lg rounded-lg">
+        <div className="card-body">
+          <div className="flex justify-between items-center gap-2">
+            <SearchBar
+              searchQuery={searchQuery}
+              setSearchQuery={setSearchQuery}
+            />
+            <SortingDropdown setSortBy={setSortBy} />
+          </div>
+          <ul>
+            {todos.length > 0 ? (
+              todos.map((todo) => (
+                <TodoItem
+                  key={todo.id}
+                  todo={todo}
+                  toggleTodoCompletion={toggleTodoCompletion}
+                  deleteTodo={deleteTodo}
+                  handleEdit={handleEdit}
+                />
+              ))
+            ) : (
+              <li className="p-12 text-center">
+                <p className="text-base-content/70 font-medium">
+                  No Todos yet, Create your first task above !
+                </p>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+      <EditModal
+        dialogRef={dialogRef}
+        dialog={dialog}
+        setDialog={setDialog}
+        saveTodo={saveTodo}
+      />
+    </section>
+  );
+}
